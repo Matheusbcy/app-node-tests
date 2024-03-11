26cb5213ac97a072bf57f4d06b26b5bc8763a777
Matheusbcy <matheusbcy@gmail.com>
2024-03-11 09:04:43 -0300

    organizando arquivos

---
+++ src/routes/user.js
@@ -0,0 +1,11 @@
+module.exports = () => {
+  const findAll = (req, res) => {
+    const users = [{ name: "John Doe", email: "Johndoe@gmail.com" }];
+    res.status(200).json(users);
+  };
+  const create = (req, res) => {
+    res.status(201).json(req.body);
+  };
+
+  return { findAll, create };
+};
